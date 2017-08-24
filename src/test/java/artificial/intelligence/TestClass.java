8ac3441cd79092d8b02950e07fec158d0f624d52
Atti <barbocz.attila@gmail.com>
2017-08-24 19:18:29 +0200

    classification

---
+++ src/test/java/artificial/intelligence/TestClass.java
@@ -0,0 +1,10 @@
+package artificial.intelligence;
+
+public class TestClass {
+    public int x = 0;
+    public int y = 0;
+    public TestClass(int a, int b) {
+        x = a;
+        y = b;
+    }
+}
